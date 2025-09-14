2b4159787f27492d9c7f8bf2e3bd63111d22d4b6
yashchainani21 <ychainani21@gmail.com>
2025-09-14 13:07:29 -0500

    debugging generation of PKS products

--- scripts/01_generate_unbound_PKS_products.py
+++ scripts/01_generate_unbound_PKS_products.py
@@ -1,10 +1,10 @@
+import bcs
 import pickle
-from typing import List
-from rdkit import Chem
-
 from rdkit import Chem
 from rdkit.Chem import AllChem
-from typing import List
+from typing import List, Dict
+
+remove_stereochemistry = True
 
 def run_pks_release_reaction(pks_release_mechanism: str,
                              bound_product_mol: Chem.Mol) -> List[Chem.Mol]:
@@ -54,7 +54,63 @@ def run_pks_release_reaction(pks_release_mechanism: str,
 
     raise ValueError(f"Unsupported PKS release mechanism: {pks_release_mechanism}")
 
-
 with open("../data/raw/bound_PKS_products_3_ext_mods.pkl", "rb") as f:
     bound_PKS_products = pickle.load(f)
 
+PKS_designs = [bound_PKS_products[i][0] for i in range(len(bound_PKS_products))]
+bound_PKS_mols = [bound_PKS_products[i][1] for i in range(len(bound_PKS_products))]
+
+unbound_PKS_products_dict: Dict[bcs.Cluster, Chem.Mol] = {}
+unique_PKS_product_smiles = set()
+
+for i, bound_PKS_mol in enumerate(bound_PKS_mols):
+    PKS_design = PKS_designs[i]
+
+    # try a thiolysis reaction to offload the PKS product
+    try:
+        unbound_PKS_products_list: List[Chem.Mol] = run_pks_release_reaction("thiolysis", bound_PKS_mol)
+        
+        # for each product generated from thiolysis
+        for unbound_PKS_product_mol in unbound_PKS_products_list:
+
+            # remove stereochemistry from resulting carboxylic acid product if specified
+            if remove_stereochemistry:
+                Chem.RemoveStereochemistry(unbound_PKS_product_mol)
+            
+            unbound_PKS_product_smiles = Chem.MolToSmiles(unbound_PKS_product_mol)
+            unique_PKS_product_smiles.add(unbound_PKS_product_smiles)
+            
+            # if each carboxylic acid product is unique, save the PKS design and its corresponding product SMILES
+            if unbound_PKS_product_smiles not in unbound_PKS_products_dict:
+                unbound_PKS_products_dict[PKS_design] = unbound_PKS_product_smiles
+                print(unbound_PKS_products_dict)
+                exit()
+
+    except Exception as e:
+        print(f"Error in thiolysis for {PKS_design}: {e}")
+        continue
+
+    # try a cyclization reaction to offload the PKS product
+    try:
+        unbound_PKS_products_list: List[Chem.Mol] = run_pks_release_reaction("cyclization", bound_PKS_mol)
+
+        # for each product generated from cyclization
+        for unbound_PKS_product_mol in unbound_PKS_products_list:
+            unbound_PKS_product_smiles = Chem.MolToSmiles(unbound_PKS_product_mol)
+            
+            # remove stereochemistry from resulting cyclized product if specified
+            if remove_stereochemistry:
+                Chem.RemoveStereochemistry(unbound_PKS_product_mol)
+
+            unbound_PKS_product_smiles = Chem.MolToSmiles(unbound_PKS_product_mol)
+            unique_PKS_product_smiles.add(unbound_PKS_product_smiles)
+
+            # if each cyclized product is unique, save the PKS design and its corresponding product SMILES
+            if unbound_PKS_product_smiles not in unbound_PKS_products_dict:
+                unbound_PKS_products_dict[PKS_design] = unbound_PKS_product_smiles
+
+    except Exception as e:
+        print(f"Error in cyclization for {PKS_design}: {e}")
+        continue
+
+print(f"Generated {len(unbound_PKS_products_dict)} unique unbound PKS products.\n")
