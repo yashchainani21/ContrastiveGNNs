b6a208634adb0cedf21920ee86cd8deb41ed1b3a
yashchainani21 <ychainani21@gmail.com>
2025-09-14 10:09:09 -0500

    modify starters and extendesr

--- scripts/00_generate_bound_PKS_products.py
+++ scripts/00_generate_bound_PKS_products.py
@@ -1,1 +1,28 @@
-import retrotide
+from typing import List, Optional
+from retrotide import retrotide, structureDB
+import bcs
+
+def modify_bcs_starters_extenders(starter_codes: Optional[List[str]] = None,
+                                  extender_codes: Optional[List[str]] = None):
+    """
+    Modify the starter and extender acyl-CoA units used to generate PKS products
+    """
+    if starter_codes is not None:
+        for key in list(bcs.starters.keys()):
+            if key not in starter_codes:
+                bcs.starters.pop(key, None) # removes key corresponding to starter code that was not specified
+
+    if extender_codes is not None:
+        for key in list(bcs.extenders.keys()):
+            if key not in extender_codes:
+                bcs.extenders.pop(key, None) # removes key corresponding to extender code that was not specified
+
+# allow for all starter units to be used in generatation of PKS products
+starter_codes = None 
+
+# for extenders, only allow Malonyl-CoA and Methylmalonyl-CoA
+extender_codes = ['Malonyl-CoA', 'Methylmalonyl-CoA']
+
+modify_bcs_starters_extenders(starter_codes = starter_codes, extender_codes = extender_codes)
+print((f"\nNumber of starter units: {len(bcs.starters)}"))
+print((f"\nNumber of extender units: {len(bcs.extenders)}\n"))
