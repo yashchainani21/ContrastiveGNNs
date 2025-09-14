08a01f60c6bec169591d73f939afaba0ec03fb87
yashchainani21 <ychainani21@gmail.com>
2025-09-14 10:39:13 -0500

    update script description

--- scripts/00_generate_bound_PKS_products.py
+++ scripts/00_generate_bound_PKS_products.py
@@ -1,10 +1,22 @@
 from typing import List, Optional
+from collections import OrderedDict
 import bcs
 
 def modify_bcs_starters_extenders(starter_codes: Optional[List[str]] = None,
                                   extender_codes: Optional[List[str]] = None):
     """
     Modify the starter and extender acyl-CoA units used to generate PKS products
+
+    Parameters
+    ----------
+    starter_codes : Optional[List[str]], optional
+        List of starter unit codes to be used. If None, all starter units are used.
+    extender_codes : Optional[List[str]], optional
+        List of extender unit codes to be used. If None, all extender units are used.
+
+    Returns
+    -------
+    None
     """
     if starter_codes is not None:
         for key in list(bcs.starters.keys()):
@@ -34,8 +46,25 @@ print(f"\nNumber of entries in structureDB: {len(structureDB)}\n")
 # this can be changed to allow for more stereoisomers to be generated later on
 allowed_KR_subtypes = ['B1', 'B']
 
-def build_bcs_cluster_and_product(starter, extension_mods_combo):
+def build_bcs_cluster_and_product(starter: str, 
+                                  extension_mods_combo):
     """
     Build a bcs PKS cluster and its corresponding PKS product given a starter unit and extension module
     """
-    
+    try:
+        # build loading module
+        loading_AT_domain = bcs.AT(active = True, substrate = starter)
+        loading_module = bcs.Module(domains = OrderedDict({bcs.AT: loading_AT_domain}), loading = True)
+        full_modules = [loading_module] + list(extension_mods_combo)
+
+        # create bcs cluster
+        cluster = bcs.Cluster(modules = full_modules)
+
+        # generate PKS product
+        product_mol = cluster.computeProduct(structureDB)
+
+        return cluster, product_mol
+
+    except Exception as e:
+        print(f"Error building loading module with starter {starter} combo {extension_mods_combo}: {e}")
+        return None, None
