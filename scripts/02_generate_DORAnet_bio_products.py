2da17ddda7b57bd7adaaa63e666af8b69fbb0078
yashchainani21 <ychainani21@gmail.com>
2025-09-15 09:33:11 -0500

    modify in and out paths

--- scripts/02_generate_DORAnet_bio_products.py
+++ scripts/02_generate_DORAnet_bio_products.py
@@ -8,14 +8,17 @@ RDLogger.DisableLog('rdApp.*')
 
 if __name__ == '__main__':
 
+    # define PKS products to modify
+    max_extension_modules = 3
+
     # MPI initialization
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
 
     # define filepaths for PKS unbound PKS products
-    precursors_filepath = '../data/interim/unbound_PKS_products_3_ext_mods_no_stereo.pkl'
-    output_filepath = '../data/interim/DORAnet_BIO1_from_PKS_products_3_ext_mods_no_stereo.txt'
+    precursors_filepath = f'../data/interim/unbound_PKS_products_{max_extension_modules}_ext_mods_no_stereo.pkl'
+    output_filepath = f'../data/interim/DORAnet_BIO1_from_PKS_products_{max_extension_modules}_ext_mods_no_stereo.txt'
 
     # only rank 0 reads files and sets up initial data
     if rank == 0:
@@ -24,4 +27,3 @@ if __name__ == '__main__':
         with open(precursors_filepath, 'rb') as precursors_file:
             unbound_PKS_products = pd.read_pickle(precursors_file)
             precursors_list = [smi for smi in unbound_PKS_products.values()]
-            print(precursors_list)
